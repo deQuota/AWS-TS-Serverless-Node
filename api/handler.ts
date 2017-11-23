7ee89b6df0a2924c6b6a6b6d0534cbd58fd56af0
deQuota <nisalwaduge@gmail.com>
2017-11-23 15:43:12 +0530

    added Angular 4

---
+++ api/handler.ts
@@ -0,0 +1,26 @@
+
+import { Dynamo} from "./dynamodb/helper";
+import * as Winston from 'winston';
+
+
+export const hello = async (event, context, cb) => {
+    let { clientId } = event.pathParameters;
+    const params = {
+        TableName: 'Clients',
+            Key: {
+
+                clientId: clientId
+            },
+    },
+        dynamo = new Dynamo();
+        dynamo.getDocClient().get(params, (err, data) => {
+            Winston.info('Get Client table item', JSON.stringify(data, null, 2));
+
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify('Tenant ID for clientID '+clientId+' = '+data.Item['fmtenantId']),
+            };
+
+            cb(null, response);
+    });
+};
