7d5b0f0ed9b00b4ca3a6b760701a5f82bca1e116
deQuota <nisalwaduge@gmail.com>
2017-12-03 23:44:56 +0530

    added 'back to world map'

--- web/src/app/flight-search/flight-search.component.ts
+++ web/src/app/flight-search/flight-search.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FlightsService} from '../common/services/flights.service';
+
 /*import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';*/
 
 @Component({
@@ -13,13 +14,14 @@ export class FlightSearchComponent implements OnInit {
   selectedFlight: any;
   flightSelected = false;
   iconUrl;
-  polyLinePoints:any = [];
-  template: string =`<img src="http://pa1.narvii.com/5722/2c617cd9674417d272084884b61e4bb7dd5f0b15_hq.gif" />`;
-  j=0;
+  polyLinePoints: any = [];
+  template: string = `<img src="http://pa1.narvii.com/5722/2c617cd9674417d272084884b61e4bb7dd5f0b15_hq.gif" />`;
+  j = 0;
 
   constructor(private flightService: FlightsService) {
 
   }
+
   ngOnInit() {
 
 
@@ -55,33 +57,37 @@ export class FlightSearchComponent implements OnInit {
 
 
   }
-  getFlightOnce(icao: string){
+
+  getFlightOnce(icao: string) {
 
 
     this.flightService.getSelectedFlight(icao).subscribe(
-
       response => {
         this.flightSelected = true;
         this.selectedFlight = response.message;
-       /* this.spinnerService.hide();*/
+        /* this.spinnerService.hide();*/
         /*this.polyLinePoints[0] = { lat:this.selectedFlight[0][6], lon:this.selectedFlight[0][5]};*/
-        this.polyLinePoints[this.j] = { lat:this.selectedFlight[0][6], lon:this.selectedFlight[0][5]};
+        this.polyLinePoints[this.j] = {lat: this.selectedFlight[0][6], lon: this.selectedFlight[0][5]};
         this.j++;
-        if(this.selectedFlight[0][10]>0 && this.selectedFlight[0][10]<91){
+        if (this.selectedFlight[0][10] > 0 && this.selectedFlight[0][10] < 91) {
           this.iconUrl = 'assets/icons/flight045.png';
         }
-        else if(this.selectedFlight[0][10]>90 && this.selectedFlight[0][10]<181){
+        else if (this.selectedFlight[0][10] > 90 && this.selectedFlight[0][10] < 181) {
           this.iconUrl = 'assets/icons/flight135.png';
         }
-        else if(this.selectedFlight[0][10]>180 && this.selectedFlight[0][10]<271){
+        else if (this.selectedFlight[0][10] > 180 && this.selectedFlight[0][10] < 271) {
           this.iconUrl = 'assets/icons/flight225.png';
         }
-        else{
+        else {
           this.iconUrl = 'assets/icons/flight315.png';
         }
         console.log('Flight Selected >>' + this.selectedFlight[0][0]);
       }
     );
+  }
+
+  onCilckBackToWM() {
+    location.reload();
   }
 
 }
